e5cc091ba4d881d78258a3fbf5213260ffcee418
arifomer733 <mcsf19m027@pucit.edu.pk>
2021-06-05 04:05:41 +0500

    Final Year Project

---
+++ app/src/main/java/com/example/parkit/userScreen.java
@@ -0,0 +1,16 @@
+package com.example.parkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class userScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_screen);
+
+    }
+}
